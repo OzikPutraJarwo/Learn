db6c91534611c8f825394e01cc934ce1d9aa4445
Vina Cantik <vinamafaza21@gmail.com>
2024-06-20 19:46:22 +0700

    Perbaikan Klast dan PCA

--- R/FIX/PCA.r
+++ R/FIX/PCA.r
@@ -1,5 +1,5 @@
 library(readxl)
-Data_path_new <- read_excel("assets/Data Total No R.xlsx", 
+Data_path_new <- read_excel("R/assets/Data Total No R.xlsx", 
   col_types = c("text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 library(factoextra)
@@ -16,7 +16,7 @@ pca <- fviz_pca_biplot(res.pca,
                       col.ind = c("#67a9cf","#d73027", "#1b7837"),
                       # Variable
                       geom.var = c("text"),
-                      col.var = "#8c5e00",
+                      col.var = "#851b85",
                       habillage = Data_path_new$Genotipe,
                       xlab = "PC1 (42.1%)",
                       ylab = "PC2 (22.9%)"
