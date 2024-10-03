99783dee37622fed3e56dc273add87dea7224abc
Ozik Jarwo <jarwoozik08@gmail.com>
2024-10-03 17:47:57 +0700

    Kontak & link

--- App/Bengkel Bang Jarwo/src/js/func.js
+++ App/Bengkel Bang Jarwo/src/js/func.js
@@ -1,7 +1,7 @@
 const headerOffset = 5.75 * 16; // 6rem dalam piksel (16px adalah ukuran font dasar)
 
 document.querySelectorAll('[data-scroll]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('data-scroll');
         const targetElement = document.querySelector(targetId);
@@ -13,4 +13,10 @@ document.querySelectorAll('[data-scroll]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
+});
+
+document.querySelector('#order .kirim-pesan').addEventListener('click', function () {
+    var nama = encodeURIComponent(document.querySelector('#order input').value);
+    var pesan = encodeURIComponent(document.querySelector('#order textarea').value);
+    window.open('https://api.whatsapp.com/send?phone=6285654141926&text=Halo%20Bang%20Jarwo,%20saya%20' + nama + '%20ingin%20bertanya.%0A%0A' + pesan + '%0A%0ATerima%20kasih.');
 });
