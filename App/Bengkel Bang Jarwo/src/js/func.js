45a1261b6364c6afc485a60c98825cfdb3ff116d
Ozik Jarwo <jarwoozik08@gmail.com>
2024-10-03 22:52:39 +0700

    Responsive dan final

--- App/Bengkel Bang Jarwo/src/js/func.js
+++ App/Bengkel Bang Jarwo/src/js/func.js
@@ -1,4 +1,4 @@
-const headerOffset = 5.75 * 16; // 6rem dalam piksel (16px adalah ukuran font dasar)
+const headerOffset = 5.5 * 16; // 6rem dalam piksel (16px adalah ukuran font dasar)
 
 document.querySelectorAll('[data-scroll]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -7,7 +7,6 @@ document.querySelectorAll('[data-scroll]').forEach(anchor => {
         const targetElement = document.querySelector(targetId);
         const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
         const offsetPosition = elementPosition - headerOffset;
-
         window.scrollTo({
             top: offsetPosition,
             behavior: 'smooth'
@@ -19,4 +18,12 @@ document.querySelector('#order .kirim-pesan').addEventListener('click', function
     var nama = encodeURIComponent(document.querySelector('#order input').value);
     var pesan = encodeURIComponent(document.querySelector('#order textarea').value);
     window.open('https://api.whatsapp.com/send?phone=6285654141926&text=Halo%20Bang%20Jarwo,%20saya%20' + nama + '%20ingin%20bertanya.%0A%0A' + pesan + '%0A%0ATerima%20kasih.');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileNav = document.querySelector('header .mobile-nav');
+    const leftNav = document.querySelector('header .left nav');
+    mobileNav.addEventListener('click', function() {
+        leftNav.classList.toggle('show');
+    });
 });
