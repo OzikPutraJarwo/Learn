6af784fe86bdbb630f189146abd5813b806d79cd
Ozik Jarwo <jarwoozik08@gmail.com>
2024-10-25 20:21:42 +0700

    💥 Big updates: Image watermark release, DOI to APA bug fixes, etc

--- App/tools/src/script.js
+++ App/tools/src/script.js
@@ -6,7 +6,7 @@ const setActive = (navClass, sectionId) => {
 	document.getElementById(sectionId).classList.add('active');
 };
 
-['home', 'unit-converter', 'bulk-file-renamer', 'pdf-combiner', 'image-converter', 'image-compressor', 'qr-generator', 'doi-to-apa', 'file-analyzer'].forEach(name => {
+['home', 'unit-converter', 'bulk-file-renamer', 'pdf-combiner', 'image-converter', 'image-compressor', 'image-watermark', 'qr-generator', 'doi-to-apa', 'file-analyzer'].forEach(name => {
 	document.querySelectorAll(`.${name}`).forEach(el => {
 		el.addEventListener('click', () => setActive(name, name));
 	});
@@ -81,3 +81,71 @@ function copyAll(selector) {
 			showMessage("Copied!");
 	}
 }
+
+// Function per Features
+
+let bfrInputFiles = document.getElementById('bfr-input-files');
+function bfrAddFiles() {
+	const files = bfrInputFiles.files;
+	const newFiles = Array.from(files);
+	renamedFiles = renamedFiles.concat(newFiles.map(file => ({ name: file.name, originalFile: file })));
+	bfrUpdateFiles();
+};
+
+let iwmupload = document.getElementById('iwm-input-files');
+let iwmcanvas = document.getElementById('iwm-canvas');
+let imwuploadedImage = new Image();
+iwmupload.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      imwuploadedImage.src = event.target.result;
+      imwuploadedImage.onload = () => {
+        iwmcanvas.width = imwuploadedImage.width;
+        iwmcanvas.height = imwuploadedImage.height;
+        posX = iwmcanvas.width / 2;
+        posY = iwmcanvas.height / 2;
+        drawImageWithWatermark();
+      };
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+const pdfInput = document.getElementById('pdc-input-files');
+const pdcfileList = document.getElementById('pdc-file-list');
+const combineBtn = document.getElementById('pdc-combine');
+const outputFileName = document.getElementById('pdc-file-name');
+let pdcfiles = [];
+let pdcthumbnails = [];
+pdfInput.addEventListener('change', async (event) => {
+	pdcfileList.innerHTML = "Loading...";
+	const newFiles = Array.from(event.target.files);
+	for (const file of newFiles) {
+			pdcfiles.push(file);
+			const thumbnailSrc = await createThumbnail(file);
+			pdcthumbnails.push(thumbnailSrc);
+	}
+	displayFiles();
+	combineBtn.disabled = pdcfiles.length === 0; 
+});
+
+const qrgInputs = document.querySelectorAll(`
+  #qrg-width,#qrg-height,#qrg-data,#qrg-margin,
+  #qrg-bgselector,#qrg-bgcolor,#qrg-bggradA,#qrg-bggradB,#qrg-bggradT,#qrg-bggradR,
+  #qrg-qrdensity,#qrg-qrmode,#qrg-qrerror,
+  #qrg-imgbgdot,#qrg-imgsize,#qrg-imgmargin,
+  #qrg-dotsselector,#qrg-dotscolor,#qrg-dotsgradA,#qrg-dotsgradB,#qrg-dotsgradT,#qrg-dotsgradR,#qrg-dotstype,
+  #qrg-csselector,#qrg-cscolor,#qrg-csgradA,#qrg-csgradB,#qrg-csgradT,#qrg-csgradR,#qrg-cstype,
+  #qrg-cdselector,#qrg-cdcolor,#qrg-cdgradA,#qrg-cdgradB,#qrg-cdgradT,#qrg-cdgradR,#qrg-cdtype
+  `);
+const qrgvalues = {};
+qrgInputs.forEach(input => {
+  qrgvalues[input.id] = input.value;
+  input.addEventListener("input", () => {
+    qrgvalues[input.id] = input.value;
+    console.log(`${input.id}: ${qrgvalues[input.id]}`);
+    generateQR();
+  });
+});
